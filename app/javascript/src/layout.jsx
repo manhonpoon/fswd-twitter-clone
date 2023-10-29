dc8085405bc1f8c38a0e5271bd8edf84072e6622
Christine <cyo15ch@gmail.com>
2023-10-29 11:06:52 +0800

    2023_10_29

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,96 +1,110 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { safeCredentialsFormData, handleErrors } from './utils/fetchHelper';
 
-let formData = new FormData();
-
-formData.set('tweet[image]', fileInputElement.files[0]);
-// Set other params in the form data.
-formData.set('tweet[message]', 'a tweet');
-
-fetch('/api/tweets', safeCredentialsFormData({
-  method: 'POST',
-  body: formData,
-}))
-.then(handleErrors)
-.then(res => {   console.log(res); })
-
 const Layout = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const formData = new FormData();
+    formData.set('username', username);
+    formData.set('password', password);
+
+    // Perform the login fetch request here
+    fetch('/api/login', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Handle the login response from the backend
+      })
+      .catch(error => {
+        console.error(error);
+        // Handle any errors that occurred during the request
+      });
+  };
+
+  const handleSignUp = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const formData = new FormData();
+    formData.set('username', username);
+    formData.set('email', email);
+    formData.set('password', password);
+
+    // Perform the sign-up fetch request here
+    fetch('/api/signup', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Handle the sign-up response from the backend
+      })
+      .catch(error => {
+        console.error(error);
+        // Handle any errors that occurred during the request
+      });
+  };
+
   return (
     <React.Fragment>
-      <div id="homeback">
-        </div>
-        <nav className="navbar navbar-default navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </div>
-            <ul className="nav navbar-nav navbar-right">
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong><span className="caret"></span></a>
-                <ul className="dropdown-menu row" role="menu">
-                  <li className="col-xs-12"><a href="#">Bahasa Malaya</a></li>
-                  <li className="col-xs-12"><a href="#">Dansk</a></li>
-                  <li className="col-xs-12"><a href="#">English</a></li>
-                  <li className="col-xs-12"><a href="#">Suomi</a></li>
-                </ul>
-              </li>
-            </ul>
+      {/* Existing JSX code */}
+      {/* ... */}
+
+      {/* Log-in form */}
+      <div className="log-in col-xs-4 col-xs-offset-1">
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control username"
+              placeholder="Username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </div>
-        </nav>
-        <div className="main">
-          <div className="container">
-            <div className="row">
-              <div className="front-card col-xs-10 col-xs-offset-1">
-                <div className="col-xs-6 welcome">
-                  <div id="welcome-text">
-                    <h1><strong>Welcome to Twitter.</strong></h1>
-                    <p>Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
-                  </div>
-                  <p><a href="#" id="twit-info">Hack Pacific - Backendium Twitter Project</a></p>
-                  <p><a href="#" id="twit-account">Tweet and photo by @Hackpacific<br/>3:20 PM - 15 December 2016</a></p>
-                </div>
-                <div className="log-in col-xs-4 col-xs-offset-1">
-                  <form>
-                    <div className="form-group">
-                    <input type="text" className="form-control username" placeholder="Username" />
-                    </div>
-                    <div className="form-group col-xs-8">
-                    <input type="password" className="form-control password" placeholder="Password" />
-                    </div>
-                    <button id="log-in-btn" className="btn btn-default btn-primary col-xs-3 col-xs-offset-1">Log in</button>
-                    <label>
-                    <input type="checkbox" />
-                      <span>Remember me</span>
-                      <span> &#183; </span>
-                    </label>
-                    <a href="#">Forgot password?</a>
-                  </form>
-                </div>
-                <div className="sign-up col-xs-4 col-xs-offset-1">
-                  <form>
-                    <div className="new-to-t">
-                      <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
-                    </div>
-                    <div className="form-group">
-                      <input type="text" className="form-control username" placeholder="Username" />
-                    </div>
-                    <div className="form-group">
-                      <input type="email" className="form-control email" placeholder="Email" />
-                    </div>
-                    <div className="form-group">
-                      <input type="password" className="form-control password" placeholder="Password" />
-                    </div>
-                    <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
-                  </form>
-                </div>
-              </div>
-            </div>
+          <div className="form-group col-xs-8">
+            <input
+              type="password"
+              className="form-control password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
-        </div>
+          <button
+            id="log-in-btn"
+            className="btn btn-default btn-primary col-xs-3 col-xs-offset-1"
+            type="submit"
+          >
+            Log in
+          </button>
+          <label>
+            <input type="checkbox" />
+            <span>Remember me</span>
+            <span> &#183; </span>
+          </label>
+          <a href="#">Forgot password?</a>
+        </form>
+      </div>
+
+      {/* Sign-up form */}
+      <div className="sign-up col-xs-4 col-xs-offset-1">
+        <form onSubmit={handleSignUp}>
+          {/* Form fields */}
+        </form>
+      </div>
+
+      {/* Remaining JSX code */}
+      {/* ... */}
     </React.Fragment>
-    );
-}
+  );
+};
+
 export default Layout;
